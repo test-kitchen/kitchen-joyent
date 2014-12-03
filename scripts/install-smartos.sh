dabfe57dcea86d62fb864ec5c9c68c078a543501
Sean OMeara <someara@opscode.com>
2014-12-03 15:00:56 -0500

    Update install-smartos.sh

--- scripts/install-smartos.sh
+++ scripts/install-smartos.sh
@@ -1,4 +1,5 @@
 pkgin -y install ruby-2.1.4
 pkgin -y install gcc49
 pkgin -y install gmake
-gem install chef --version 11.16.0 --no-ri --no-rdoc
+gem install chef --version 11.16.4 --no-ri --no-rdoc
+gem install builder --no-ri --no-rdoc
