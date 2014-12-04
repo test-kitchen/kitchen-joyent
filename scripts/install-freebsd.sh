a7ccaa535eb2a947d768fd5417bafc4dee350b5e
Sean OMeara <someara@opscode.com>
2014-12-04 14:05:16 -0500

    Update install-freebsd.sh

--- scripts/install-freebsd.sh
+++ scripts/install-freebsd.sh
@@ -1,3 +1,3 @@
-su - root -c 'pkg install bash'
-su - root -c 'pkg install -y curl'
-su - root -c 'curl -L www.opscode.com/chef/install.sh | sh'
+sh -c 'pkg install bash'
+sh -c 'pkg install -y curl'
+sh -c 'curl -L www.opscode.com/chef/install.sh | sh'
