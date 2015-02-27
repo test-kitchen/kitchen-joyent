38cd69fe16684275430c5b5c11ba73d6e942e081
Jared Everett <jared.everett@tcnp3.com>
2015-02-26 17:04:41 -0700

    Test default_networks

--- lib/kitchen/driver/joyent.rb
+++ lib/kitchen/driver/joyent.rb
@@ -87,7 +87,7 @@ module Kitchen
           dataset:          config[:joyent_image_id],
           package:          config[:joyent_flavor_id],
           name:             config[:joyent_image_name],
-          networks:         config[:joyent_networks],
+          default_networks: config[:joyent_networks],
           )
       end
 
